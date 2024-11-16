0d2ca071f2e284f5e2bc2a4333224f44fcd84545
Carlos Vol <76893477+CarlosVolweides@users.noreply.github.com>
2024-11-16 13:27:00 -0400

    Add files via upload

---
+++ vistas/inicio.php
@@ -0,0 +1,401 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <title>Nelas Design</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+         user-scalable=no, initial-scale=1, maximum-scale=1, minimun-scale=1">
+         <link rel="stylesheet" href="./css/style.css">
+
+         <link rel="stylesheet" 
+         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" 
+         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+         crossorigin="anonymous" 
+         referrerpolicy="no-referrer" />
+
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+    </head>
+    <body>
+        <main>
+            <div class="inicial">
+                <img src="./img/nelas1.jpg" class="imagen">
+            </div>
+            <div class="categorias_debajo">
+                <div class="columna_categoria">
+                    <ul>
+                        <div class="imagenes">
+                            <img src="./img/categoria_tops.jpg">
+                            <img src="./img/categoria_body.jpg">
+                            <img src="./img/categoria_vestidos.jpg">
+                            <img src="./img/categoria_pantalones.jpg">
+                            <img src="./img/categoria_camisas.jpg">
+                        </div>
+                    </ul>
+                    <ul>
+                        <a href="" id="tops">tops</a>
+                        <a href="" id="bodys">bodys</a>
+                        <a href="" id="vestidos">vestidos</a>
+                        <a href="" id="pantalones">pantalones</a>
+                        <a href="" id="camisas">camisas</a>
+                    </ul>
+                </div>
+            </div>
+            <div class="main-content">
+                <p id="explora">Explora nuestra colección de ropa femenina</p>
+                <div class="product-container">
+                    <div class="product0">
+                    <a href="perfil.html"><img src="./img/bodycuellocuadrado.webp" alt=""></a>
+                    <div class="product" data-name="p1">
+                        <h3>body cuello cuadrado</h3>
+                        <p>5.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/toptubocorto.webp" alt=" "/></a>
+                    <div class="product" data-name="p2">
+                        <h3>top tubo corto</h3>
+                        <p>3.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/camisaconcuellov.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p3">
+                        <h3>camisa cuello en v</h3>
+                        <p>5.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/camisacuellodetortuga.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p4">
+                        <h3>camisa cuello de tortuga</h3>
+                        <p>6.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/jogger.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p5">
+                        <h3>jogger unisex</h3>
+                        <p>8.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/vestido tubo casual.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p6">
+                        <h3>vestido de tubo casual</h3>
+                        <p>6.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/vestidocuellocuadrado.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p7">
+                        <h3>vestido cuello cuadrado</h3>
+                        <p>7.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/camisa manga larga.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p8">
+                        <h3>camisa manga larga</h3>
+                        <p>5.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/camisa cuello cuadrado.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p9">
+                        <h3>camisa cuello cuadrado</h3>
+                        <p>6.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                    <div class="product0">
+                        <a href=""><img src="./img/top halter.webp" alt="Espacio para imagen"></a>
+                    <div class="product" data-name="p10">
+                        <h3>top halter</h3>
+                        <p>4.00$</p>
+                        <button><i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="products-preview">
+                <div class="preview" data-target="p1">
+                    <i class="fa-solid fa-x" id="x"></i>
+                    <ul>
+                        <li>
+                    <img src="./img/bodycuellocuadrado.webp">
+                    </li>
+                    </ul>
+                    <ul class="text">
+                        <li>
+                    <h3>body cuello cuadrado</h3>
+                    <p id="descripcion">Body cuello cuadrado stretch elavorado en tela microdurazno</p>
+                    <div class="price">5.00$</div>
+                    <div class="color">
+                        <label>color</label>
+                        <br>
+                        <ul>
+                            <li>
+                                <button id="negro"></button>
+                                <button id="rojo"></button>
+                                <button id="blanco"></button>
+                                <button id="lila"></button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tallas">
+                        <p>tallas</p>
+                        <ul>
+                            <li>
+                                <button>XS</button>
+                                <button>S</button>
+                                <button>M</button>
+                                <button>L</button>
+                                <button>XL</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="botones">
+                        <button class="cartbtn">AÑADIR AL CARRITO</button>
+                        <button class="favbtn"> <i class="fa-regular fa-heart"></i></button>
+                    </div>
+                    </li>
+                    </ul>
+                </div>
+                <div class="preview" data-target="p2">
+                    <i class="fa-solid fa-x" id="x"></i>
+                    <ul>
+                        <li>
+                    <img src="./img/toptubocorto.webp">
+                    </li>
+                    </ul>
+                    <ul class="text">
+                    <li>
+                    <h3>top tubo corto</h3>
+                    <p id="descripcion">top tipo tubo stretch realizado con tela microdurazno</p>
+                    <div class="price">3.00$</div>
+                    <div class="color">
+                    <label>color</label>
+                    <br>
+                        <ul>
+                            <li>
+                                <button id="negro"></button>
+                                <button id="rojo"></button>
+                                <button id="blanco"></button>
+                                <button id="lila"></button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tallas">
+                        <p>tallas</p>
+                        <ul>
+                            <li>
+                                <button>XS</button>
+                                <button>S</button>
+                                <button>M</button>
+                                <button>L</button>
+                                <button>XL</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="botones">
+                        <button class="cartbtn">AÑADIR AL CARRITO</button>
+                        <button class="favbtn"> <i class="fa-regular fa-heart"></i></button>
+                    </div>
+                    </li>
+                    </ul>
+                </div>
+                <div class="preview" data-target="p3">
+                    <i class="fa-solid fa-x" id="x"></i>
+                    <ul>
+                        <li>
+                    <img src="./img/camisaconcuellov.webp">
+                    </li>
+                    </ul>
+                    <ul class="text">
+                    <li>
+                    <h3>camisa cuello en v</h3>
+                    <p id="descripcion">camisa con escote cruzado en v manga larga</p>
+                    <div class="price">5.00$</div>
+                    <div class="color">
+                        <label>color</label>
+                        <br>
+                        <ul>
+                            <li>
+                                <button id="negro"></button>
+                                <button id="rojo"></button>
+                                <button id="blanco"></button>
+                                <button id="lila"></button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tallas">
+                        <p>tallas</p>
+                        <ul>
+                            <li>
+                                <button>XS</button>
+                                <button>S</button>
+                                <button>M</button>
+                                <button>L</button>
+                                <button>XL</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="botones">
+                        <button class="cartbtn">AÑADIR AL CARRITO</button>
+                        <button class="favbtn"> <i class="fa-regular fa-heart"></i></button>
+                    </div>
+                    </li>
+                    </ul>
+                </div>
+                <div class="preview" data-target="p4">
+                    <img src="./img/camisacuellodetortuga.webp">
+                    <h3>camisa cuello de tortuga</h3>
+                    <p>camisa manga larga cuello de tortuga realizada en tela microdurazno</p>
+                    <div class="price">6.00$</div>
+                    <div class="color">
+                        <label>color</label>
+                        <ul>
+                            <li>
+                                <button id="negro">negro</button>
+                                <button id="rojo">rojo</button>
+                                <button id="blanco">blanco</button>
+                                <button id="lila">lila</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tallas">
+                        <p>tallas</p>
+                        <ul>
+                            <li>
+                                <button>XS</button>
+                                <button>S</button>
+                                <button>M</button>
+                                <button>L</button>
+                                <button>XL</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="botones">
+                        <button>AÑADIR AL CARRITO</button>
+                        <button> <i class="fa-regular fa-heart"></i></button>
+                    </div>
+                </div>
+                <div class="preview" data-target="p5">
+                    <img src="./img/jogger.webp">
+                    <h3>jogger unisex</h3>
+                    <p>jogger para damas y caballeros</p>
+                    <div class="price">8.00$</div>
+                    <div class="color">
+                        <ul>
+                            <li>
+                                <button id="negro">negro</button>
+                                <button id="rojo">rojo</button>
+                                <button id="blanco">blanco</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tallas">
+                        <p>tallas</p>
+                        <ul>
+                            <li>
+                                <button>XS</button>
+                                <button>S</button>
+                                <button>M</button>
+                                <button>L</button>
+                                <button>XL</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="botones">
+                        <button>AÑADIR AL CARRITO</button>
+                        <button> <i class="fa-regular fa-heart"></i></button>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script>
+            const list = document.querySelectorAll('.list');
+            function activeLink(){
+                list.forEach((item) => 
+                item.classList.remove('active'));
+                this.classList.add('active');
+            }
+            list.forEach((item) => 
+            item.addEventListener('click', activeLink));
+        </script>
+        <script class="articulospreview">
+            let previewContainer= document.querySelector('.products-preview');
+            let previewBox = previewContainer.querySelectorAll('.preview');
+            document.querySelectorAll('.product2 ,.product').forEach(product2 =>{
+                product2.onclick = () =>{
+                    previewContainer.style.display ='flex';
+                    let name = product2.getAttribute('data-name');
+                    previewBox.forEach(preview =>{
+                        let target = preview.getAttribute('data-target');
+                        if(name == target){
+                            preview.classList.add('active');
+                        }
+                    });
+                };
+            });
+            previewBox.forEach(close =>{
+                close.querySelector('#x').onclick = () =>{
+                    close.classList.remove('active');
+                    previewContainer.style.display ='none';
+                };
+            })
+        </script>
+        <footer>
+            <div class="nav-phone">
+                <ul>
+                    <li class="list active">
+                        <a href="">
+                            <span class="icon">
+                                <i class="fa-solid fa-house"></i>
+                            </span>
+                            <span class="text">inicio</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="categorias.html">
+                            <span class="icon">
+                                <i class="fa-solid fa-list"></i>
+                            </span>
+                            <span class="text">categorias</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="carrito.html">
+                            <span class="icon">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            </span>
+                            <span class="text">carrito
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <a href="perfil.html">
+                            <span class="icon">
+                                <i class="fa-solid fa-circle-user"></i>
+                            </span>
+                            <span class ="text">yo</span>
+                        </a>
+                    </li>
+                    <div class="indicator"></div>
+                </ul>
+            </div>
+            </div>
+        </footer>
+    </body>
+</html>
