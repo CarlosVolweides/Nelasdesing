83958ef2d7e746d62914f6d254cd4c1f343791e9
Carlos Vol <76893477+CarlosVolweides@users.noreply.github.com>
2024-11-16 13:28:24 -0400

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,41 @@
+<header>
+            <nav>
+                <div class="nav-left">
+                    <img src="./img/logo.png" class="logo">
+                    <div class="menu">
+                    <ul>
+                        <li><a href=""><strong>Inicio</strong></a></li>
+                        <li><a href="categorias.html">Comprar</a>
+                        <ul>
+                            <li><a href="">Pantalones</a></li>
+                            <li><a href="">Tops</a></li>
+                            <li><a href="">Camisas</a></li>
+                            <li><a href="">Bodys</a></li>
+                            <li><a href="">Vestidos</a></li>
+                        </ul>
+                    </li>
+                    </ul>
+                    <i class="fa-solid fa-bars" id="bars"></i>
+                    <div class="search-bar">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Buscar por modelo, categoria y mas">
+                    </div>
+                </div>
+                </div>
+                <div class="nav-right">
+                    <div>
+                        <i class="fa-solid fa-circle-user"></i>
+                        <p><a href="perfil.html">perfil</a></p>
+                    </div>
+                    <div id="fav">
+                        <i class="fa-regular fa-heart"></i>
+                        <p><a href="favoritos.html" id>favoritos</a></p>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-cart-shopping"></i>
+                        <p><a href="carrito.html">carrito</a></p>
+
+                    </div>
+                </div>
+            </nav>
+        </header>
